c0bf2555f6fe255ff5ff9128cd9cc6524de1b207
rodriguezaj2 <rodriguezaj2020@gmail.com>
2025-06-14 13:15:07 -0400

    add B login

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -24,6 +24,16 @@ public class LoginPage extends CommonMethods {
     @FindBy (xpath="//h1[text()='Dashboard']")
     public WebElement loginMessage;
 
+    @FindBy(id = "txtUsername")
+    public WebElement userName;
+
+
+    @FindBy(id = "txtPassword")
+    public WebElement password;
+
+    @FindBy(xpath = "//h1[text()='Dashboard']")
+    public WebElement dashboard;
+
     public LoginPage(){
         PageFactory.initElements(driver,this);
     }
--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -67,4 +67,6 @@ public class LoginSteps extends CommonMethods {
     }
 
 
+
+
 }
